506f529718d087e0791fbb00fbed8fe32d798189
drapello <demetrius.rapello@gmail.com>
2019-09-21 11:50:17 +0200

    fixing the nesting problem

--- words_count.py
+++ words_count.py
@@ -9,10 +9,34 @@ def word_count(string):
 	dictionary = {}
 	for word in words:
 		dictionary[word] = words.count(word)
-		return dictionary
+	return dictionary
 
 print(word_count("To be or not to be"))
 
 #The expected result should be:
 #{'to': 2, 'be': 2, 'or': 1, 'not': 1}
 # but what I get is only : 'to': 2
+
+
+
+"""
+the following implementation is an optimization of the code above.
+the above code makes use of words.count which is an internal loop on all the list's elements
+that makes the count slow.
+
+in the second implementation, I am taking the advantage of the fact that I am already going througth the list once and
+counting the elements when they appear. When I first find a word I create an entry in the dictionary initializing it with 1, 
+later if they are found again I just need to increment the word count by 1.
+"""
+def word_count2(string):
+	words = string.lower().split()
+	dictionary = {}
+	for word in words:
+		if word in dictionary:
+			dictionary[word] += 1
+		else:
+			dictionary[word] = 1
+
+	return dictionary
+
+print(word_count2("To be or not to be"))
